15ceb09b45482053e9a7351f7683a50c7c91891c
meisReal <simas.cesnakauskas@gmail.com>
2022-10-25 11:49:55 +0300

    Simple REST API that GETs all members from JSON file

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+//!
+//?
+//*
+//TODO 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html '));
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
+//* simple REST API that returns JSON
+// Gets all members
+app.get('/api/members', (req, res) => {
+    //! no need to use JSON.stringify, express takes care of it
+    res.json(members);
 });
+
+//* set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const PORT = process.env.PORT || 3000;
 
