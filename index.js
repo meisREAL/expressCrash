b9024078468d7b4d1437f37d3a0923c5ba278db6
meisReal <simas.cesnakauskas@gmail.com>
2022-10-25 12:44:22 +0300

    GET method for a single member request

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ app.get('/api/members', (req, res) => {
     res.json(members);
 });
 
+// GET single member
+app.get('/api/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+
+    if (found) {
+        //* req.params.stuff lets reach to request parameters
+        //! parseInt is used because req.params returns a string and we use ===
+        res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({ message: `No member with id ${req.params.id}` });
+    }
+
+});
+
 //* set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
